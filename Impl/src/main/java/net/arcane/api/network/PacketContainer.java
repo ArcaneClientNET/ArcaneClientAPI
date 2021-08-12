725a8f3f2448de60f0719b5e6bab1b0e6c41b232
Braydon <xcoolmanxx@gmail.com>
2021-08-12 18:32:43 -0400

    oops, forgot to actually write the packet :facepalm:

--- Impl/src/main/java/net/arcane/api/network/PacketContainer.java
+++ Impl/src/main/java/net/arcane/api/network/PacketContainer.java
@@ -14,6 +14,7 @@ public final class PacketContainer {
 	public PacketContainer(ArcanePacket packet) {
 		byteBuf = Unpooled.buffer();
 		writeInt(PacketRegistry.lookupId(packet.getClass()));
+		packet.write(this);
 	}
 	
 	/**
